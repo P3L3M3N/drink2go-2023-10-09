57e7e7551e7fc37fdae54cde3495daad5783840f
Aleksandr Lobov <kz7055917158@gmail.com>
2023-10-10 17:14:50 +0300

    feat: Implement mobile header layout with styles and interaction logic

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,19 @@
-/* в этот файл добавляет скрипты*/
+const toggle = document.querySelector('.menu-toggle');
+const menuList = document.querySelector('.main-menu-list');
+const burgerIcon = document.querySelector('.menu-toggle__icon--burger');
+const crossIcon = document.querySelector('.menu-toggle__icon--cross');
+const menuName = document.querySelector('.menu-toggle__name');
+
+toggle.addEventListener('click', function() {
+    if (menuName.textContent === 'Закрыть основное меню') {
+        menuList.style.display = 'none';
+        burgerIcon.style.display = 'block';
+        crossIcon.style.display = 'none';
+        menuName.textContent = 'Открыть основное меню';
+    } else {
+        menuList.style.display = 'block';
+        burgerIcon.style.display = 'none';
+        crossIcon.style.display = 'block';
+        menuName.textContent = 'Закрыть основное меню';
+    }
+});
