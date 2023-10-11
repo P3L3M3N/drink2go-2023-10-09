a479eb541ad77a5c6ea4d276f64a31a8a83a850f
Aleksandr Lobov <kz7055917158@gmail.com>
2023-10-11 18:27:10 +0300

    feat: Integrate site-wide svg-sprites and add 'pros' section markup/styles

--- source/js/script.js
+++ source/js/script.js
@@ -5,18 +5,19 @@ const crossIcon = document.querySelector('.menu-toggle__icon--cross');
 const menuName = document.querySelector('.menu-toggle__name');
 
 toggle.addEventListener('click', function() {
-    if (menuName.textContent === 'Закрыть основное меню') {
-        menuList.style.display = 'none';
-        burgerIcon.style.display = 'block';
-        crossIcon.style.display = 'none';
-        menuName.textContent = 'Открыть основное меню';
-    } else {
+    if (menuName.textContent === 'Открыть основное меню') {
         menuList.style.display = 'block';
         burgerIcon.style.display = 'none';
         crossIcon.style.display = 'block';
         menuName.textContent = 'Закрыть основное меню';
+    } else {
+        menuList.style.display = 'none';
+        burgerIcon.style.display = 'block';
+        crossIcon.style.display = 'none';
+        menuName.textContent = 'Открыть основное меню';
     }
 });
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
